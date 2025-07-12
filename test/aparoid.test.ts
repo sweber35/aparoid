6736250ca02cb551fa116021786c3ac1bd08105c
Sean Weber <sweber@pltw.org>
2025-07-11 22:01:54 -0400

    Initial commit

---
+++ test/aparoid.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Aparoid from '../lib/aparoid-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aparoid-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Aparoid.AparoidStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
